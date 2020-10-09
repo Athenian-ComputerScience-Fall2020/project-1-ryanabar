d8b6cf1fc583528802c2d794e16f25db97d22759
ryanabar <21rabar@athenian.org>
2020-10-09 20:16:27 +0000

    Submit before lunch

--- my_code.py
+++ my_code.py
@@ -1,8 +1,29 @@
-# Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+def yogurtshack():
+    
+    print("Welcome to yogurt shack! Please type your order below.")
+
+    flavor = input("what would u like ur falvor to be")
+    topping1 = input("topping 1?")
+    topping2 = input("topping 2?")
+
+    lst = [flavor, topping1, topping2]
+
+    print("Your order is" + str(lst))
 
 
+    x = 0
+    while x<5:
+        print("hi")
+        x += 1
 
+    if flavor == "chocolate":
+        print("ew")
+    else:
+        print("good choice")
 
-
+    for i in range(2):
+        print("hi")
+    
+yogurtshack()
+yogurtshack()
 
